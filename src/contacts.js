53b8a8a72f9c3aa1da733f4e8cc7fd214c88c2fa
mansuroff123 <mansuroff145@gmail.com>
2024-08-03 09:09:39 +0500

    test

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+const contacts = [
+    {
+        name:"Elena Charles",
+        imgURL: "",
+        phone: "+987 765 432",
+        email: "Charles@gmail.com"
+    },
+    {
+        name:"Tristan Patton",
+        imgURL: "",
+        phone: "+123 456 789",
+        email: "Patton@gmail.com"
+    },
+    {
+        name:"Malachi Bowman",
+        imgURL: "",
+        phone: "+563 876 234",
+        email: "Bowman@gmail.com"
+    }
+]
+
+export default contacts;
